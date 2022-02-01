837c11eb75df95bfe7fa8426bad7f7db74116929
amine.im <amineprojet7@gmail.com>
2022-02-01 08:15:01 +0100

    remove name in logs
    Huh, it's your project and all are know is made by you, Why you put ur name in logs? that's make no sense.

--- client.lua
+++ client.lua
@@ -1,10 +1,10 @@
 local function Debug(data, state)
-    print("[qb-debug - By MaDHouSe] - Start Debug")
+    print("[qb-debug] - Start Debug")
     if state then
         print(json.encode(data, {indent = true})) 
     else
         print(data) 
     end
-    print("[qb-debug - By MaDHouSe] - End Debug")
+    print("[qb-debug] - End Debug")
 end
 exports("Debug", Debug)
